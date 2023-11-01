f907b16ae3d6d5cad752e2aa2604e5437c3aa5f1
eeezi <alloy0035@hanmail.net>
2023-11-02 03:05:03 +0900

    이전 회차 코드 공유

---
+++ src/main/java/com/hello1/hellospring1/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package com.hello1.hellospring1.repository;
+
+import com.hello1.hellospring1.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+}
